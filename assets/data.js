ba8bc1a0f53711975d0a47be87a245c661595dea
Utsav Sharma <utsavsharma@Utsavs-MBP.lan>
2024-05-06 00:43:06 -0400

    Folders

---
+++ assets/data.js
@@ -0,0 +1,622 @@
+var markers = [
+  {
+    "FIRE_YEAR": 1999,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 158000,
+    "LATITUDE": 26.2337,
+    "LONGITUDE": -80.7665,
+    "STATE": "FL"
+  },
+  {
+    "FIRE_YEAR": 2002,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 259158.9,
+    "LATITUDE": 34.1084,
+    "LONGITUDE": -110.4859,
+    "STATE": "AZ"
+  },
+  {
+    "FIRE_YEAR": 2002,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 209704.1,
+    "LATITUDE": 34.1362,
+    "LONGITUDE": -110.7029,
+    "STATE": "AZ"
+  },
+  {
+    "FIRE_YEAR": 2003,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 91281,
+    "LATITUDE": 34.19694444,
+    "LONGITUDE": -117.2761111,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2009,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 160371,
+    "LATITUDE": 34.2425,
+    "LONGITUDE": -118.1888889,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 1996,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 83323,
+    "LATITUDE": 39.25833333,
+    "LONGITUDE": -122.9166667,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2012,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 107847,
+    "LATITUDE": 39.3231,
+    "LONGITUDE": -112.3881,
+    "STATE": "UT"
+  },
+  {
+    "FIRE_YEAR": 2000,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 83508,
+    "LATITUDE": 43.73166667,
+    "LONGITUDE": -103.8866667,
+    "STATE": "SD"
+  },
+  {
+    "FIRE_YEAR": 2002,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 189688,
+    "LATITUDE": 62.6994,
+    "LONGITUDE": -155.6523,
+    "STATE": "AK"
+  },
+  {
+    "FIRE_YEAR": 2013,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Arson/incendiarism",
+    "CAUSE__ABRV": "arson_incendiarism",
+    "FIRE_SIZE": 87154.4,
+    "LATITUDE": 64.68,
+    "LONGITUDE": -146.58,
+    "STATE": "AK"
+  },
+  {
+    "FIRE_YEAR": 2006,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Debris and open burning",
+    "CAUSE__ABRV": "debris_and_open_burning",
+    "FIRE_SIZE": 162702,
+    "LATITUDE": 34.63222222,
+    "LONGITUDE": -118.7697222,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2001,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Debris and open burning",
+    "CAUSE__ABRV": "debris_and_open_burning",
+    "FIRE_SIZE": 84730,
+    "LATITUDE": 64.3329,
+    "LONGITUDE": -148.9524,
+    "STATE": "AK"
+  },
+  {
+    "FIRE_YEAR": 2006,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Debris and open burning",
+    "CAUSE__ABRV": "debris_and_open_burning",
+    "FIRE_SIZE": 118520.1,
+    "LATITUDE": 64.37,
+    "LONGITUDE": -149.03,
+    "STATE": "AK"
+  },
+  {
+    "FIRE_YEAR": 2011,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 122500,
+    "LATITUDE": 33.11793333,
+    "LONGITUDE": -100.325,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2007,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 240207,
+    "LATITUDE": 34.77916667,
+    "LONGITUDE": -120.09,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 1996,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 106668,
+    "LATITUDE": 35.42,
+    "LONGITUDE": -120.4227778,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2017,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 83599,
+    "LATITUDE": 40.07486,
+    "LONGITUDE": -120.14819,
+    "STATE": "NV"
+  },
+  {
+    "FIRE_YEAR": 2012,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 146832,
+    "LATITUDE": 43.71111111,
+    "LONGITUDE": -115.3655556,
+    "STATE": "ID"
+  },
+  {
+    "FIRE_YEAR": 1996,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 118230,
+    "LATITUDE": 44.9832,
+    "LONGITUDE": -121.3512,
+    "STATE": "OR"
+  },
+  {
+    "FIRE_YEAR": 2000,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 81220,
+    "LATITUDE": 46.20297,
+    "LONGITUDE": -111.27376,
+    "STATE": "MT"
+  },
+  {
+    "FIRE_YEAR": 2016,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 171915,
+    "LATITUDE": 46.569326,
+    "LONGITUDE": -120.080828,
+    "STATE": "WA"
+  },
+  {
+    "FIRE_YEAR": 2000,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 163884,
+    "LATITUDE": 46.57194444,
+    "LONGITUDE": -119.7727778,
+    "STATE": "WA"
+  },
+  {
+    "FIRE_YEAR": 2015,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 218138,
+    "LATITUDE": 48.33814,
+    "LONGITUDE": -119.00175,
+    "STATE": "WA"
+  },
+  {
+    "FIRE_YEAR": 2002,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Equipment and vehicle use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 115328,
+    "LATITUDE": 65.4376,
+    "LONGITUDE": -148.8181,
+    "STATE": "AK"
+  },
+  {
+    "FIRE_YEAR": 2017,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Firearms and explosives use",
+    "CAUSE__ABRV": "equipment_and_vehicle_use",
+    "FIRE_SIZE": 81826,
+    "LATITUDE": 37.61965,
+    "LONGITUDE": -120.2158,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2011,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 175000,
+    "LATITUDE": 30.1635,
+    "LONGITUDE": -101.2471667,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2008,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 98168,
+    "LATITUDE": 30.63117,
+    "LONGITUDE": -101.5342,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2007,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 191474,
+    "LATITUDE": 30.80277778,
+    "LONGITUDE": -82.2725,
+    "STATE": "GA"
+  },
+  {
+    "FIRE_YEAR": 2011,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 314444,
+    "LATITUDE": 30.84951667,
+    "LONGITUDE": -104.0460222,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2008,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 220000,
+    "LATITUDE": 31.74216,
+    "LONGITUDE": -101.0438,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2011,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 222954,
+    "LATITUDE": 31.81972222,
+    "LONGITUDE": -109.2111111,
+    "STATE": "AZ"
+  },
+  {
+    "FIRE_YEAR": 2003,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 82440,
+    "LATITUDE": 32.43,
+    "LONGITUDE": -110.7716667,
+    "STATE": "AZ"
+  },
+  {
+    "FIRE_YEAR": 2006,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 92390,
+    "LATITUDE": 33.05825,
+    "LONGITUDE": -103.6025,
+    "STATE": "NM"
+  },
+  {
+    "FIRE_YEAR": 2011,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 89200,
+    "LATITUDE": 33.60996575,
+    "LONGITUDE": -100.6924122,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2017,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 281893,
+    "LATITUDE": 34.42527778,
+    "LONGITUDE": -119.0602778,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2009,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 91622,
+    "LATITUDE": 34.94972222,
+    "LONGITUDE": -119.9777778,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2006,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 427696,
+    "LATITUDE": 35.21666667,
+    "LONGITUDE": -100.9333333,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2017,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 135000,
+    "LATITUDE": 35.31808333,
+    "LONGITUDE": -100.5388333,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2006,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 479549,
+    "LATITUDE": 35.6678,
+    "LONGITUDE": -101.3708,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2017,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 318156,
+    "LATITUDE": 36.0865,
+    "LONGITUDE": -100.531,
+    "STATE": "TX"
+  },
+  {
+    "FIRE_YEAR": 2013,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 255858,
+    "LATITUDE": 37.86055556,
+    "LONGITUDE": -120.0830556,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2014,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 97717,
+    "LATITUDE": 38.7925,
+    "LONGITUDE": -120.6022222,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2002,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 137760,
+    "LATITUDE": 39.0575,
+    "LONGITUDE": -105.4083333,
+    "STATE": "CO"
+  },
+  {
+    "FIRE_YEAR": 2017,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 98960.1,
+    "LATITUDE": 39.762111,
+    "LONGITUDE": -119.252656,
+    "STATE": "NV"
+  },
+  {
+    "FIRE_YEAR": 2010,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 108855,
+    "LATITUDE": 43.52969,
+    "LONGITUDE": -112.88078,
+    "STATE": "ID"
+  },
+  {
+    "FIRE_YEAR": 2016,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 188404,
+    "LATITUDE": 43.95027778,
+    "LONGITUDE": -115.7619444,
+    "STATE": "ID"
+  },
+  {
+    "FIRE_YEAR": 2013,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 80053,
+    "LATITUDE": 47.2982,
+    "LONGITUDE": -120.1225,
+    "STATE": "WA"
+  },
+  {
+    "FIRE_YEAR": 2017,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 127498,
+    "LATITUDE": 48.85527778,
+    "LONGITUDE": -120.4136111,
+    "STATE": "WA"
+  },
+  {
+    "FIRE_YEAR": 2015,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 75135,
+    "LATITUDE": 62.58,
+    "LONGITUDE": -158.02,
+    "STATE": "AK"
+  },
+  {
+    "FIRE_YEAR": 2001,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Missing data/not specified/undetermined",
+    "CAUSE__ABRV": "missing_data",
+    "FIRE_SIZE": 112112,
+    "LATITUDE": 64.6618,
+    "LONGITUDE": -148.2055,
+    "STATE": "AK"
+  },
+  {
+    "FIRE_YEAR": 2007,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Power generation/transmission/distribution",
+    "CAUSE__ABRV": "power_generation",
+    "FIRE_SIZE": 197990,
+    "LATITUDE": 33.07444444,
+    "LONGITUDE": -116.7111111,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2011,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Power generation/transmission/distribution",
+    "CAUSE__ABRV": "power_generation",
+    "FIRE_SIZE": 156593,
+    "LATITUDE": 35.811667,
+    "LONGITUDE": -106.536667,
+    "STATE": "NM"
+  },
+  {
+    "FIRE_YEAR": 2007,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 90440,
+    "LATITUDE": 32.608768,
+    "LONGITUDE": -116.577142,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2003,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 280059,
+    "LATITUDE": 32.98555556,
+    "LONGITUDE": -116.7275,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2011,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 88835,
+    "LATITUDE": 33.14369,
+    "LONGITUDE": -108.36869,
+    "STATE": "NM"
+  },
+  {
+    "FIRE_YEAR": 2011,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 538049,
+    "LATITUDE": 33.60611111,
+    "LONGITUDE": -109.4497222,
+    "STATE": "AZ"
+  },
+  {
+    "FIRE_YEAR": 2002,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 150696,
+    "LATITUDE": 35.87361111,
+    "LONGITUDE": -118.4580556,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2008,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 81378,
+    "LATITUDE": 36.10527778,
+    "LONGITUDE": -121.4308333,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2016,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 132127,
+    "LATITUDE": 36.46083333,
+    "LONGITUDE": -121.9005556,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2012,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 75431,
+    "LATITUDE": 40.00694444,
+    "LONGITUDE": -121.2713889,
+    "STATE": "CA"
+  },
+  {
+    "FIRE_YEAR": 2007,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 75484,
+    "LATITUDE": 48.06888889,
+    "LONGITUDE": -90.81138889,
+    "STATE": "MN"
+  },
+  {
+    "FIRE_YEAR": 2010,
+    "NWCG_CAUSE_CLASSIFICATION": "Human",
+    "NWCG_GENERAL_CAUSE": "Recreation and ceremony",
+    "CAUSE__ABRV": "recreation_and_ceremony",
+    "FIRE_SIZE": 91885,
+    "LATITUDE": 62.395,
+    "LONGITUDE": -153.5033,
+    "STATE": "AK"
+  }
+];
